f69e7cac7d81fc47ecfbb01c64d4c292138d60f1
Rafalrrr159 <rafal.rrr555@gmail.com>
2026-02-17 16:29:13 +0100

    feat: Apply initial migration and create database

---
+++ KanbanApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,36 @@
+﻿using KanbanApi.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace KanbanApi.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Board> Boards { get; set; }
+        public DbSet<Column> Columns { get; set; }
+        public DbSet<Card> Cards { get; set; }
+        public DbSet<BoardMember> BoardMembers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<BoardMember>()
+                .HasKey(bm => new { bm.UserId, bm.BoardId });
+
+            modelBuilder.Entity<BoardMember>()
+                .HasOne(bm => bm.User)
+                .WithMany(u => u.BoardMemberships)
+                .HasForeignKey(bm => bm.UserId);
+
+            modelBuilder.Entity<BoardMember>()
+                .HasOne(bm => bm.Board)
+                .WithMany(b => b.Members)
+                .HasForeignKey(bm => bm.BoardId);
+        }
+    }
+}
